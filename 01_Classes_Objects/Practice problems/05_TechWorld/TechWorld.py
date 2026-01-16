7d7f55aec45d1de8e5061130bd03a1406266d2b5
Siddharth <siddharth.mane29s@gmail.com>
2026-01-16 11:10:25 +0000

    add TechWorld

--- 01_Classes_Objects/Practice problems/05_TechWorld/TechWorld.py
+++
@@ -1,4 +0,0 @@
-# allocate courses to the instructors
-# instructor has -> name , tech skills , experience, avg feedback 
-# instructor gets a course if : 
-# 
---
+++ 01_Classes_Objects/Practice problems/05_TechWorld/solution.py
@@ -0,0 +1,35 @@
+# allocate courses to the instructors
+# instructor has -> name , tech skills , experience, avg feedback  -> all private 
+# eligiblity -> 
+# 
+
+class TechWorld:
+    def __init__(self,name,technology,experience,feeback):
+        self.__name = name
+        self.__technologyskill = technology
+        self.__experience = experience
+        self.__avgfeedback = feeback
+        
+        
+    def check_eligibility(self):
+        if self.__experience >3 and self.__avgfeedback>=4.5 :
+            return True 
+        elif self.__experience <3 and self.__avgfeedback>=4:
+            return True
+        else :
+            return False
+
+    def allocate_course(self,subject):
+        if self.check_eligibility():
+            if subject in self.__technologyskill:
+                print('course allocated')
+            else:
+                print('cannot allocate course')
+        
+p1 = TechWorld('sid',['Django','Machine Learning'],10,4.5)
+p1.allocate_course('SpringBoot')
+
+    
+
+
+
