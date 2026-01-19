1fe0bf41d53b30f711de6fa286bc51d38b886482
Siddharth <siddharth.mane29s@gmail.com>
2026-01-19 18:12:18 +0000

    add vehicle solutions

---
+++ 03_Inheritence/Practice Problems/Vehicle/solution.py
@@ -0,0 +1,21 @@
+class Vehicle:
+    def __init__(self,seating):
+        self.seating = seating
+
+    def fare(self):
+        return self.seating*100
+
+class Bus(Vehicle):
+    def __init__(self, seating=50):
+        super().__init__(seating)
+    
+    def fare(self):
+        base_fare  = super().fare()
+        extra =  (0.1*base_fare)
+        return base_fare+extra
+        
+    
+v1 = Vehicle(10)
+print(v1.fare())
+b1 = Bus()
+print(b1.fare())
